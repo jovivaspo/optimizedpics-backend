d480b2cd26ac250158fb29ae0719bc7b9e782915
jovivaspo <jovivaspo@gmail.com>
2023-03-06 07:30:26 +0100

    Adding public content

--- src/app.js
+++ src/app.js
@@ -7,8 +7,10 @@ const app = express();
 
 app.set("port", process.env.PORT);
 
-app.use(helmet());
 app.use(cors());
+
+app.use(helmet());
+
 app.use(express.json());
 
 app.use(express.static("public"));
