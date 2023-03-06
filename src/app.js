956d5f4807a2a15689a637a06ba3d3d6457f9e3c
Jorge Vivas <jovivaspo@gmail.com>
2023-03-06 10:12:31 +0100

    Adding cors

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const app = express();
 app.set("port", process.env.PORT);
 
 //app.use(helmet());
-//app.use(cors());
+app.use(cors());
 app.use(express.json());
 
 app.use(express.static("public"));
